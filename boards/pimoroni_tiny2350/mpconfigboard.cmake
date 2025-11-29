cc57d8c02597f2a4b0d2028da7b6c871862ebac4
Olly Butterfield <olly@opb.me.uk>
2025-11-29 20:07:43 +0000

    Update mpconfigboard.cmake

--- boards/pimoroni_tiny2350/mpconfigboard.cmake
+++ boards/pimoroni_tiny2350/mpconfigboard.cmake
@@ -5,6 +5,10 @@ set(PICO_PLATFORM "rp2350")
 # Board specific version of the frozen manifest
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
 
+# Links micropy_lib_lwip and sets MICROPY_PY_LWIP = 1
+# Picked up and expanded upon in mpconfigboard.h
+set(MICROPY_PY_LWIP ON)
+
 set(MICROPY_C_HEAP_SIZE 4096)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
